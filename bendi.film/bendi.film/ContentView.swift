cdcf52ca0e0fc171391faeaff70c4a77f51a7566
rishabh b <rishabh@rishabhs-MacBook-Air.local>
2025-04-16 11:04:02 -0400

    commiting first Xcode project

---
+++ bendi.film/bendi.film/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bendi.film
+//
+//  Created by rishabh b on 4/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bendi.film/bendi.film/bendi_filmApp.swift
@@ -0,0 +1,17 @@
+//
+//  bendi_filmApp.swift
+//  bendi.film
+//
+//  Created by rishabh b on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct bendi_filmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
